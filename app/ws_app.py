fbdfa94bbcae0c132d32d8223ef41bc86a4931f4
Arina <arina@example.com>
2026-08-02 21:28:29 +0500

    62

--- app/ws_app.py
+++ app/ws_app.py
@@ -1,4 +1,7 @@
 print("🔥🔥🔥 WS_APP.PY LOADED 🔥🔥🔥")
+if __name__ != "__main__":
+    raise RuntimeError("ws_app.py must not be imported by Flask")
+
 import sys
 import os
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
