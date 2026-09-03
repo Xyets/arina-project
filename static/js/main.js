adf63236c54446cfae0d47ee2cb4e70cf7eedc87
Arina <arina@example.com>
2026-09-03 22:04:54 +0500

    правила красота 6.3

--- static/js/main.js
+++ static/js/main.js
@@ -789,3 +789,30 @@ window.toggleWheel = (ruleId) => {
     box.classList.toggle("show");
 };
 
+const typeSelect = document.getElementById("typeSelect");
+const typeDisplay = document.getElementById("typeDisplay");
+const typeOptions = document.getElementById("typeOptions");
+
+typeDisplay.addEventListener("click", () => {
+    typeOptions.style.display =
+        typeOptions.style.display === "flex" ? "none" : "flex";
+});
+
+document.querySelectorAll(".option").forEach(opt => {
+    opt.addEventListener("click", () => {
+        const value = opt.getAttribute("data-value");
+        typeDisplay.textContent = opt.textContent;
+        typeOptions.style.display = "none";
+
+        // передаём значение в твой обработчик
+        document.getElementById("new_action_type").value = value;
+        updateNewRuleFields();
+    });
+});
+
+// закрытие при клике вне меню
+document.addEventListener("click", (e) => {
+    if (!typeSelect.contains(e.target)) {
+        typeOptions.style.display = "none";
+    }
+});
